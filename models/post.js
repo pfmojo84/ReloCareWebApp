afe4ae6776cd28d35934ecb2ea802f0e732ca558
TimL94 <99834249+TimL94@users.noreply.github.com>
2024-04-09 23:52:50 -0400

    cahnged user

--- models/post.js
+++ models/post.js
@@ -26,7 +26,7 @@ Post.init(
         user_id: {
             type: DataTypes.INTEGER,
             references: {
-                model: 'User',
+                model: 'user',
                 key: 'id'
 
             }
